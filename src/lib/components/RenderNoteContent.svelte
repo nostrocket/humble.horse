9653cc9341f74fd0d3668dbc3aee7479131f59f2
gsovereignty <gsovereignty@proton.me>
2024-04-19 14:13:00 +1200

    problem: nostr:\S overflows bubble

--- src/lib/components/RenderNoteContent.svelte
+++ src/lib/components/RenderNoteContent.svelte
@@ -10,17 +10,20 @@
 		// replace newline characters with <br> tags
 		let formattedInput = input.replace(/(\r\n|\n|\r)/gm, '<br>');
 
+		const nostr = /nostr:\S+/g;
+
+		formattedInput = formattedInput.replace(nostr, (s)=>{
+			s = s.replace("nostr:", "");
+			return ` <a href="https://njump.me/${s}">njump</a> ` //todo: render mentioned notes inline, render usernames for npubs, 
+		});
+
 		// regex to find URLs
 		const urlRegex = /https?:\/\/\S+\.(jpg|jpeg|png|gif|svg)\b/g;
 
 		// Replace image URLs with <img> tags
 		formattedInput = formattedInput.replace(urlRegex, (url) => {
-			return `<img src="${url}" alt="Image" style="max-width: 98%; height: auto;" class="m-2 rounded">`;
+			return `<img src="${url}" alt="${url}" style="max-width: 98%; height: auto;" class="m-2 rounded">`;
 		});
-
-        const nostr = /nostr:\S+/g;
-
-        
 
 		return formattedInput;
 	}
