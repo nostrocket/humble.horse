864b232ec39c1a2330247563fb70d7d9e4c4d40d
gsovereignty <gsovereignty@proton.me>
2024-05-28 19:56:05 +0800

    problem: people complaining about spinners

--- src/lib/components/Coracle.svelte
+++ src/lib/components/Coracle.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div class="flex flex-col items-center justify-center gap-4 py-20 overflow-hidden">
+	WE HAVE THE MOST SPINNERS OF ANY NOSTR CLIENT
 	<div class="flex flex-row">
 		<div class="flex flex-col items-center justify-center gap-4 py-20">
 			<Circle2 size="30" unit="px" /><Circle2 size="60" unit="px" /><Circle2 size="90" unit="px" /><Circle2 size="120" unit="px" />
@@ -14,5 +15,6 @@
 			<Circle2 size="30" unit="px" /><Circle2 size="60" unit="px" /><Circle2 size="90" unit="px" /><Circle2 size="120" unit="px" />
 		</div>
 	</div>
+	(please wait, your notes are loading)
     <!-- <Circle2 size="800" unit="px" /> hodlbod's motto is "you can never have too many spinners", but I don't know about this last one, it might be too much-->
 </div>
