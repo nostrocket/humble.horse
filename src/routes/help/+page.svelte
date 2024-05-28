9088ee5bf95e8d3a029105c9563166126ddc45bf
gsovereignty <gsovereignty@proton.me>
2024-05-28 20:15:01 +0800

    problem: description not clear

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -19,11 +19,18 @@
 							class="px-3.5 py-2 bg-gray-200 dark:bg-gray-700 rounded-e-xl rounded-es-xl flex flex-col gap-2"
 						>
 							<p class="text-sm font-normal text-gray-900 dark:text-white py-2">
-								Humble Horse is an experiment in making nostr into a giant group chat. Instead of creating discrete groups, we "move" the nostr client over conversations based on heuristics to dynamically "create" groups based on what content the user is interested in. I have some ideas on how to find optimise for presenting the user with the best "groups" so keep watching the progress.
+								Humble Horse is an experiment in making nostr into a giant group chat with no global state. Instead of creating discrete groups like telegram, every nostr thread is a group.
+							</p>
+							<p class="text-sm font-normal text-gray-900 dark:text-white py-2">
+								The problem with telegram groups is the velocity is either too slow to be interesting or too fast to be useful.
+							</p>
+
+							<p class="text-sm font-normal text-gray-900 dark:text-white py-2">
+								The goal here is to be able to "move" the nostr client throughout the nostrverse to dynamically create "groups" with the right velocity of content, and to present the user with the best "groups" based on the interests (as demonstrated through their engagement with others).
 							</p>
 
                             <p class="text-sm font-normal text-gray-900 dark:text-white py-2">
-								Bloom filter events are used to prevent the user seeing "groups" that they are not interest in more than once.
+								Bloom filter events (NIP coming soon) are used to prevent the user seeing "groups" more than once (if they have scrolled past them and shown no interest).
 							</p>
 
 							<p class="text-sm font-normal text-gray-900 dark:text-white py-2">
